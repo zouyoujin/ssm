6b9e0cc62dfd07c6cd1f7b65603a51cc53ab74d8
zouyoujin <zouyoujin168@gmail.com>
2017-05-15 01:44:00 +0800

    首页代码优化

--- webui/src/main/resources/js/pages/App.tsx
+++ webui/src/main/resources/js/pages/App.tsx
@@ -8,35 +8,16 @@ import { Router, Route, Link, hashHistory, IndexRoute, Redirect, browserHistory,
 import { Menu, Icon } from 'antd';
 
 // pages
-import Index from './demo/Index';
+import Home from './Home';
 import Page1 from './demo/Page1';
 import Page2 from './demo/Page2';
 import Page3 from './demo/Page3';
-
-export default class Application extends Component<any, any> {
-  render() {
-    return (
-      <div>
-        <div className="header">
-          <Link to="page1" activeClassName="active">page1</Link>
-          <Link to="page2">page2</Link>
-          <Link to="page3">page3</Link>
-        </div>
-        {this.props.children}
-        <Index/>
-      </div>
-    );
-  }
-}
+import UserAddPage from './user/UserAdd';
 
 // 配置路由
 render((
     <Router history={hashHistory}>
-      <Route path="/" component={Application}>
-        <IndexRoute component={Page1} />
-        <Route path="page1" component={Page1}></Route>
-        <Route path="page2" component={Page2}></Route>
-        <Route path="page3" component={Page3}></Route>
-      </Route>
+        <Route path="/" component={Home}/>
+        <Route path="/user/add" component={UserAddPage}/>
     </Router>
 ), document.getElementById('app'));
---
+++ webui/src/main/resources/js/pages/user/UserAdd.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class UserAdd extends Component<any, any> {
+  render() {
+    return (
+      <div>User add page sucess.</div>
+    );
+  }
+}
