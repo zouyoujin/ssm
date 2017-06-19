3c67fd66f95b28cbbb7a44a0625efbb319811e9d
zouyoujin <zouyoujin168@gmail.com>
2017-06-20 03:04:07 +0800

    init

--- ssm.basedata.api/src/main/java/com/ssm/basedata/service/UserService.java
+++ ssm.basedata.api/src/main/java/com/ssm/basedata/service/UserService.java
@@ -1,9 +1,29 @@
 package com.ssm.basedata.service;
 
+import com.ssm.common.exception.BusinessException;
+import com.ssm.common.model.User;
+import com.ssm.common.pojo.UserTO;
+
 /**
  * 用户服务类接口
+ * 
  * @author kitty
  */
 public interface UserService {
-    
+
+	/**
+	 * 根据用户ID查询用户数据
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public User getUserById(Long id) throws BusinessException;
+
+	/**
+	 * 用户注册处理
+	 * 
+	 * @param userTO
+	 * @return
+	 */
+	public User registerUser(UserTO userTO);
 }
